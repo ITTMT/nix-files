63f4180add96af7ed39ddc548fbc3f8aa44f7d4d
ITTMT <oliver.iasmith@gmail.com>
2025-05-02 17:08:42 +0100

    Setting up flake system

--- flake.nix
+++ flake.nix
@@ -5,11 +5,27 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    pkgs = import nixpkgs {
+      inherit system;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+      config = {
+        allowUnfree = true;
+      };
+    };
+  
+  in
+  {
+    nixosConfigurations = {
+      myNixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./nixos/configuration.nix
+        ];
+      };
+    };
   };
 }
