3dc7a5ef1b596509c69831ed083a05297e7e3355
ITTMT <oliver.iasmith@gmail.com>
2025-05-10 18:00:24 +0100

    fix gnome keyring

--- modules/keyring.nix
+++ modules/keyring.nix
@@ -6,7 +6,7 @@
   ];
 
   # Make sure the keyring daemon is started on login
-  services.gnome-keyring = {
+  services.gnome.gnome-keyring = {
     enable = true;
     components = [ "secrets" "ssh" "pkcs11" "gpg" ];
   };
