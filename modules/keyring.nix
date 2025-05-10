d5a66200df102d24f3ea86e8f888b680e68b5cdc
ITTMT <oliver.iasmith@gmail.com>
2025-05-10 19:30:43 +0100

    fix keyring

--- modules/keyring.nix
+++ modules/keyring.nix
@@ -3,10 +3,22 @@
   environment.systemPackages = with pkgs; [
     gnome-keyring
     seahorse # Optional, a GUI for managing secrets
+    libsecret
   ];
 
   # Make sure the keyring daemon is started on login
   services.gnome.gnome-keyring = {
     enable = true;
+    secrets.enable = true;
+  };
+
+  systemd.user.services.gnome-keyring-daemon = {
+    enable = true;
+    wantedBy = [ "default.target" ];
+  };
+
+  environment.variables = {
+    SSH_AUTH_SOCK = "${config.services.gnome.gnome-keyring.socketDir}/ssh";
+    GPG_AGENT_INFO = "${config.services.gnome.gnome-keyring.socketDir}/gpg";
   };
 }
