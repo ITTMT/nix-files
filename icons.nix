06bbc417583c36bdf719fbfe015e32847ab86e74
ITTMT <oliver.iasmith@gmail.com>
2025-05-11 11:25:38 +0100

    icons

--- icons.nix
+++ icons.nix
@@ -4,6 +4,6 @@
     catppuccin
     nerd-fonts
     noto-fonts-emoji  # for proper emoji rendering
-    noto-fonts-cjk    # for better unicode support
+    noto-fonts-cjk-sans # for better unicode support
   ];
 }
