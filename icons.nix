90cf2ed6ef8c72d7ecc994f87c6bbe609cfbf89c
ITTMT <oliver.iasmith@gmail.com>
2025-05-11 11:23:07 +0100

    add other icons

--- icons.nix
+++ icons.nix
@@ -2,6 +2,8 @@
 {
   environment.systemPackages = with pkgs; [
     catppuccin
+    nerd-fonts
+    noto-fonts-emoji  # for proper emoji rendering
+    noto-fonts-cjk    # for better unicode support
   ];
-  
 }
