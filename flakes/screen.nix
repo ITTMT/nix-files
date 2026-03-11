dad4fc3d6ce51da42dcd60b98e7c788d4edd9644
ITTMT <oliver.iasmith@gmail.com>
2026-03-11 19:03:37 +0000

    update brightness

--- flakes/screen.nix
+++ flakes/screen.nix
@@ -16,4 +16,8 @@
 
   boot.extraModulePackages = [ config.boot.kernelPackages.ddcci-driver ];
   boot.kernelModules = [ "i2c-dev" "ddcci-backlight" ];
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="i2c-dev", ACTION=="add", ATTR{name}=="*DDR*", ATTR{name}=="*ddc*", RUN+="${pkgs.kmod}/bin/modprobe ddcci_backlight", RUN+="${pkgs.bash}/bin/sh -c 'echo ddcci 0x37 > /sys/class/i2c-dev/%k/device/new_device'"
+  '';
 }
